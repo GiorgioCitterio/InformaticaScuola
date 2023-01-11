35f7c2ea40ed02c589a77be9a532ac9e61ba5e5e
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-01-11 19:08:48 +0100

    commit memory

--- 00_MemoryGiorgioCitterio/00_MemoryGiorgioCitterio/Facile.xaml.cs
+++ 00_MemoryGiorgioCitterio/00_MemoryGiorgioCitterio/Facile.xaml.cs
@@ -1,15 +1,19 @@
-using System.Diagnostics.Contracts;
+using System.Diagnostics;
 
 namespace _00_MemoryGiorgioCitterio;
 
 public partial class Facile : ContentPage
 {
-    static public int[,] matricePosNumeri = new int[4, 4];
+    public int[,] matricePosNumeri = new int[4, 4];
     public int contCarteGir = 0;
     public ImageButton cartaGirata;
+    public int coppieTrovate = 0;
+    public int mosse = 0;
+    public Stopwatch sw = new Stopwatch();
     public Facile()
 	{
 		InitializeComponent();
+        sw.Start();
         Random random = new Random();
         for (int i = 1; i < 9; i++)
         {
@@ -38,17 +42,29 @@ public partial class Facile : ContentPage
             return;
         }
         ImageButton image = (ImageButton)sender;
+        lblTempo.Text = "Tempo: "+sw.Elapsed.ToString();
         await image.RotateTo(180, 200);
         image.Rotation = 0;
         image.Source = "num" + matricePosNumeri[Grid.GetRow(image), Grid.GetColumn(image)].ToString() + ".jpg";
         contCarteGir++;
+        mosse++;
+        lblMosse.Text = "Mosse: "+mosse;
         if (contCarteGir >= 2)
         {
             await Task.Delay(500);
             if (matricePosNumeri[Grid.GetRow(image), Grid.GetColumn(image)] == matricePosNumeri[Grid.GetRow(cartaGirata), Grid.GetColumn(cartaGirata)])
             {
-                cartaGirata = null;
                 contCarteGir= 0;
+                image.IsEnabled= false;
+                cartaGirata.IsEnabled = false;
+                cartaGirata = null;
+                coppieTrovate++;
+                if (coppieTrovate == 8)
+                {
+                    sw.Stop();
+                    await Navigation.PushAsync(new Vittoria());
+                }
+                lblCoppieTrovate.Text = "Coppie trovate: " + coppieTrovate;
                 return;
             }
             image.Source = string.Empty;
@@ -57,5 +73,11 @@ public partial class Facile : ContentPage
         }
         else
             cartaGirata = image;
+        
+    }
+
+    private async void StopGame(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
     }
 }
---
+++ 00_MemoryGiorgioCitterio/00_MemoryGiorgioCitterio/Vittoria.xaml.cs
@@ -0,0 +1,9 @@
+namespace _00_MemoryGiorgioCitterio;
+
+public partial class Vittoria : ContentPage
+{
+	public Vittoria()
+	{
+		InitializeComponent();
+	}
+}
