9b0aaa5b3238bdf72c87ab214efe8c9ffa27ec1a
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-01-26 10:56:11 +0100

    commit icon font

---
+++ 06_MAUI/12_TabShellLayout/NewPage1.cs
@@ -0,0 +1,15 @@
+namespace _12_TabShellLayout;
+
+public class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
