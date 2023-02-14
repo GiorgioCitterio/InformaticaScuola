b74393041830ab2d17e33e74094e8c1f2e480b59
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-02-14 13:23:54 +0100

    init

---
+++ 07_ProgrammazioneDiRete/01_EsempioDNS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+namespace _01_EsempioDNS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var domainEntry = Dns.GetHostEntry("www.istitutogreppi.edu.it");
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+            var domainEntryByAddress = Dns.GetHostEntry("127.0.0.1");
+            Console.WriteLine(domainEntryByAddress.HostName);
+            foreach (var ip in domainEntryByAddress.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+        }
+    }
+}
+
+        
---
+++ 07_ProgrammazioneDiRete/02_EsempioHttpClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+namespace _02_EsempioHttpClient
+{
+    public class Program
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+        static async void Main(string[] args)
+        {
+            HttpResponseMessage response = await httpClient.GetAsync("https://www.istitutogreppi.edu.it/");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
