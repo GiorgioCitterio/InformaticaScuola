b4d70616dbc6359dd58c029120153adfc7d35851
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-02-15 12:59:01 +0100

    Lezione 15 02

---
+++ 07_ProgrammazioneDiRete/03_UsoProxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using SettingProxy;
+namespace _03_UsoProxy
+{
+    internal class Program
+    {
+        static HttpClient client;
+        static async void Main(string[] args)
+        {
+            MyProxy.HttpClientProxySetup(out client);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://google.it");
+                response.EnsureSuccessStatusCode();
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }           
+        }   
+    }
+}
---
+++ 07_ProgrammazioneDiRete/04_DownloadFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using SettingProxy;
+namespace _04_DownloadFile
+{
+    public class Program
+    {
+        static HttpClient client;
+        static async Task<int> ProcessUrlAsync(string url, HttpClient client)
+        {
+            var byteArray = await client.GetByteArrayAsync(url);
+            //visualizza risultato
+            return byteArray.Length;
+        }
+        static async Task DownloadAsync()
+        {
+            MyProxy.HttpClientProxySetup(out client);
+            client.MaxResponseContentBufferSize= 10000000;
+            Task<int> var1 = ProcessUrlAsync("https://docs.microsoft.com/en-us/welcome-to-docs", client);
+            int var2 = await ProcessUrlAsync("https://istitutogreppi.edu.it", client);
+            int var3 = await ProcessUrlAsync("https://google.it", client);
+            int totale = await var1 + var2 + var3;
+            Console.WriteLine(totale);
+        }
+        static async Task Main(string[] args)
+        {
+            await DownloadAsync();
+        }
+    }
+}
