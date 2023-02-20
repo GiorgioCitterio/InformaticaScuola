287de29c72d5379a014e957bdd22b3c253d690d1
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-02-20 10:45:43 +0100

    json

---
+++ 07_ProgrammazioneDiRete/06_MetodiDiEstensione/Program.cs
@@ -0,0 +1,25 @@
+﻿using CustomExtension;
+namespace _06_MetodiDiEstensione
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string parola = "ciao        oggi  è lunedì, è una bella giornata! come va  ? f";
+            Console.WriteLine(parola.WordCount());
+        }
+    }
+}
+
+namespace CustomExtension
+{
+    public static class StringExtension
+    {
+        public static int WordCount(this String s)
+        {
+            //return s.Split(new char[] {' ', '.', ',', ';', '?', '!'}, StringSplitOptions.RemoveEmptyEntries).Length;
+            var vettore = s.Split(new char[] { ' ', '.', ',', ';', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            return vettore.Length;
+        }
+    }
+}
---
+++ 07_ProgrammazioneDiRete/07_EsempioJSON/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07_EsempioJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
