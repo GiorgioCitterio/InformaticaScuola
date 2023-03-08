15d71cd627d6044d0aca7b98cefdbe501e74a95d
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-03-08 15:02:01 +0100

    lezione 08 03

---
+++ 07_ProgrammazioneDiRete/14_EsempioPrevisioni/Model/Forecast.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _14_EsempioPrevisioni.Model
+{
+    public class Forecast
+    {
+        public Coord? Coord { get; set; }
+        public Weather[]? Weather { get; set; }
+        [JsonPropertyName("_base")]
+        public string? Base { get; set; }
+        public Main? Main { get; set; }
+        public int? Visibility { get; set; }
+        public Wind? Wind { get; set; }
+        public Clouds? Clouds { get; set; }
+        public int? Dt { get; set; }
+        public Sys? Sys { get; set; }
+        public int? Timezone { get; set; }
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public int? Cod { get; set; }
+    }
+
+    public class Coord
+    {
+        public float? Lon { get; set; }
+        public float? Lat { get; set; }
+    }
+
+    public class Main
+    {
+        public float? Temp { get; set; }
+        [JsonPropertyName("feels_like")]
+        public float? Feels_like { get; set; }
+        [JsonPropertyName("temp_min")]
+        public float? Temp_min { get; set; }
+        [JsonPropertyName("temp_max")]
+        public float? Temp_max { get; set; }
+        public int? Pressure { get; set; }
+        public int? Humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public float? Speed { get; set; }
+        public int? Deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int? All { get; set; }
+    }
+
+    public class Sys
+    {
+        public int Type { get; set; }
+        public int Id { get; set; }
+        public string? Country { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string? Main { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+    }
+}
---
+++ 07_ProgrammazioneDiRete/14_EsempioPrevisioni/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14_EsempioPrevisioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
