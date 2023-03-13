93dec1d4b992a8d5ead5e2c5ab1a8604f93b4f49
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-03-13 11:53:31 +0100

    lezione 13 03

---
+++ 07_ProgrammazioneDiRete/14_EsempioPrevisioni/Model/OpenMeteoForecast.cs
@@ -0,0 +1,130 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _14_EsempioPrevisioni.Model
+{
+    public class Daily
+    {
+        [JsonPropertyName("time")]
+        public List<string> Time { get; set; }
+
+        [JsonPropertyName("temperature_2m_max")]
+        public List<double?> Temperature2mMax { get; set; }
+
+        [JsonPropertyName("temperature_2m_min")]
+        public List<double?> Temperature2mMin { get; set; }
+
+        [JsonPropertyName("sunrise")]
+        public List<string> Sunrise { get; set; }
+
+        [JsonPropertyName("sunset")]
+        public List<string> Sunset { get; set; }
+
+        [JsonPropertyName("windspeed_10m_max")]
+        public List<double?> Windspeed10mMax { get; set; }
+    }
+
+    public class DailyUnits
+    {
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+        [JsonPropertyName("temperature_2m_max")]
+        public string Temperature2mMax { get; set; }
+
+        [JsonPropertyName("temperature_2m_min")]
+        public string Temperature2mMin { get; set; }
+
+        [JsonPropertyName("sunrise")]
+        public string Sunrise { get; set; }
+
+        [JsonPropertyName("sunset")]
+        public string Sunset { get; set; }
+
+        [JsonPropertyName("windspeed_10m_max")]
+        public string Windspeed10mMax { get; set; }
+    }
+
+    public class Hourly
+    {
+        [JsonPropertyName("time")]
+        public List<string> Time { get; set; }
+
+        [JsonPropertyName("temperature_2m")]
+        public List<double?> Temperature2m { get; set; }
+
+        [JsonPropertyName("temperature_1000hPa")]
+        public List<double?> Temperature1000hPa { get; set; }
+
+        [JsonPropertyName("cloudcover_1000hPa")]
+        public List<int?> Cloudcover1000hPa { get; set; }
+
+        [JsonPropertyName("windspeed_1000hPa")]
+        public List<double?> Windspeed1000hPa { get; set; }
+
+        [JsonPropertyName("winddirection_1000hPa")]
+        public List<int?> Winddirection1000hPa { get; set; }
+
+        [JsonPropertyName("geopotential_height_1000hPa")]
+        public List<int?> GeopotentialHeight1000hPa { get; set; }
+    }
+
+    public class HourlyUnits
+    {
+        [JsonPropertyName("time")]
+        public string Time { get; set; }
+
+        [JsonPropertyName("temperature_2m")]
+        public string Temperature2m { get; set; }
+
+        [JsonPropertyName("temperature_1000hPa")]
+        public string Temperature1000hPa { get; set; }
+
+        [JsonPropertyName("cloudcover_1000hPa")]
+        public string Cloudcover1000hPa { get; set; }
+
+        [JsonPropertyName("windspeed_1000hPa")]
+        public string Windspeed1000hPa { get; set; }
+
+        [JsonPropertyName("winddirection_1000hPa")]
+        public string Winddirection1000hPa { get; set; }
+
+        [JsonPropertyName("geopotential_height_1000hPa")]
+        public string GeopotentialHeight1000hPa { get; set; }
+    }
+
+    public class OpenMeteoForecast
+    {
+        [JsonPropertyName("latitude")]
+        public double? Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public double? Longitude { get; set; }
+
+        [JsonPropertyName("generationtime_ms")]
+        public double? GenerationtimeMs { get; set; }
+
+        [JsonPropertyName("utc_offset_seconds")]
+        public int? UtcOffsetSeconds { get; set; }
+
+        [JsonPropertyName("timezone")]
+        public string Timezone { get; set; }
+
+        [JsonPropertyName("timezone_abbreviation")]
+        public string TimezoneAbbreviation { get; set; }
+
+        [JsonPropertyName("elevation")]
+        public int? Elevation { get; set; }
+
+        [JsonPropertyName("hourly_units")]
+        public HourlyUnits HourlyUnits { get; set; }
+
+        [JsonPropertyName("hourly")]
+        public Hourly Hourly { get; set; }
+
+        [JsonPropertyName("daily_units")]
+        public DailyUnits DailyUnits { get; set; }
+
+        [JsonPropertyName("daily")]
+        public Daily Daily { get; set; }
+    }
+}
--- 07_ProgrammazioneDiRete/14_EsempioPrevisioni/Program.cs
+++ 07_ProgrammazioneDiRete/14_EsempioPrevisioni/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Web;
@@ -51,10 +52,60 @@ namespace _14_EsempioPrevisioni
             }
         }
 
+        static async Task PrevisioniOpen()
+        {
+            string city = "Cassago Brianza";
+            string countryCode = "it";
+            (double lat, double lon)? geo = await GeocodeByOpenWeatherMap(city, countryCode);
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={geo?.lat}&longitude={geo?.lon}&hourly=temperature_2m,temperature_1000hPa,dewpoint_1000hPa,relativehumidity_1000hPa,cloudcover_1000hPa,windspeed_1000hPa,winddirection_1000hPa,geopotential_height_1000hPa&models=ecmwf_ifs04&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max,windgusts_10m_max&timezone=auto";
+            var response = await client.GetAsync($"{url}");
+            if (response.IsSuccessStatusCode)
+            {
+                OpenMeteoForecast? forecast = await response.Content.ReadFromJsonAsync<OpenMeteoForecast>();
+                if (forecast != null)
+                {
+                    Console.WriteLine($"Latitutide: {forecast.Latitude}; Longitudine: {forecast.Longitude}; Elevazione: {forecast.Elevation} m; TimeZone: {forecast.Timezone}");
+                    if(forecast.Daily != null)
+                    {
+                        Console.WriteLine("temp2min: " + forecast.Daily.Temperature2mMax + " temp2max: " + forecast.Daily.Temperature2mMin);
+                    }
+                }
+            }
+
+        }
+
+        static async Task<(double lat, double lon)?> GeocodeByOpenWeatherMap(string? city, string? countryCode, int limit = 1)
+        {
+            string? cityUrlEncoded = HttpUtility.UrlEncode(city);
+            string geocodingUrl = $"https://api.openweathermap.org/geo/1.0/direct?q={cityUrlEncoded},{countryCode}&limit={limit}&appid={openWeatherMapKey}";
+            try
+            {
+                HttpResponseMessage responseGeocoding = await client.GetAsync($"{geocodingUrl}");
+                if (responseGeocoding.IsSuccessStatusCode)
+                {
+                    List<Location>? geocodingResult = await responseGeocoding.Content.ReadFromJsonAsync<List<Location>>();
+                    if (geocodingResult != null && geocodingResult.Count > 0)
+                    {
+                        return (geocodingResult[0].Lat, geocodingResult[0].Lon);
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                if (ex is HttpRequestException || ex is ArgumentException)
+                {
+                    Debug.WriteLine(ex.Message + "\nIl recupero dei dati dal server non è riuscito");
+                }
+            }
+            return null;
+        }
+
         static async Task Main(string[] args)
         {
             MyProxy.HttpClientProxySetup(out client);
-            await PrevisioniCorrenti();
+            //await PrevisioniCorrenti();
+            await PrevisioniOpen();
         }
 
         static OpenWeatherMapStore GetDataFromStore()
