6b7946daa49a3d13a2ca3c84316618c03f041761
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2023-05-13 09:43:55 +0200

    solo cose migration

---
+++ 09_MinAPIEsercizi/EsercizioPreVerifica/Model/Regista.cs
@@ -0,0 +1,11 @@
+﻿namespace EsercizioPreVerifica.Model
+{
+    public class Regista
+    {
+        public int RegistaId { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Nazionalità { get; set; }
+        public ICollection<Film> Films { get; set; } = new HashSet<Film>();
+    }
+}
---
+++ 09_MinAPIEsercizi/EsercizioPreVerifica/Program.cs
@@ -0,0 +1,29 @@
+using EsercizioPreVerifica.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("FilmAPIConnection");
+var serverVersion = ServerVersion.AutoDetect(connectionString);
+builder.Services.AddDbContext<FilmDbContext>(
+        dbContextOptions => dbContextOptions
+            .UseMySql(connectionString, serverVersion)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors());
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
